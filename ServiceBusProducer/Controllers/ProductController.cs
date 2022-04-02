b5f0db4615d53e67826ecb8603871205f72c4baf
Estefano J C Timoteo <EstefanoTimoteo@LAPTOP-0JCJR0NA>
2022-04-02 11:11:04 -0300

    Add project files.

---
+++ ServiceBusProducer/Controllers/ProductController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceBusProducer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> AddProduct([FromBody] Product request )
+        {
+            //
+            // Write here all the code (clean architecture) needed to add a product in a database
+            //
+
+            var serviceBusClient =
+                new TopicClient
+                ("Endpoint=sb://pocsbaceleracao.servicebus.windows.net/;SharedAccessKeyName=commonuser;SharedAccessKey=DYuCO6S1QcVDVW7tiTI5KLH+37Jk/y+SJ68IH68T+eY=", // primaryConnectionString
+                 "ProductAdded");
+
+            var message = new Message(request.ToJsonBytes());
+            message.ContentType = "application/json";
+            message.UserProperties.Add("CorrelationId", Guid.NewGuid().ToString());
+            message.UserProperties.Add("Culture", Thread.CurrentThread.CurrentCulture.Name);
+            Console.WriteLine("inicio");
+
+            await serviceBusClient.SendAsync(message);
+
+            return Created("http://localhost", request);
+        }
+
+
+    }
+    public class Product
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("price")]
+        public double Price { get; set; }
+    }
+
+    public static class Utils
+    {
+        private static readonly UTF8Encoding Utf8NoBom = new UTF8Encoding(false);
+
+        private static readonly JsonSerializerSettings JsonSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            DateFormatHandling = DateFormatHandling.IsoDateFormat,
+            NullValueHandling = NullValueHandling.Ignore,
+            DefaultValueHandling = DefaultValueHandling.Ignore,
+            MissingMemberHandling = MissingMemberHandling.Ignore,
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+            TypeNameHandling = TypeNameHandling.None,
+            Converters = new JsonConverter[] { new StringEnumConverter() }
+        };
+
+        /// <summary>
+        /// Converts the object to json bytes.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <returns></returns>
+        public static byte[] ToJsonBytes(this object source)
+        {
+            if (source == null)
+                return null;
+            var instring = JsonConvert.SerializeObject(source, Formatting.Indented, JsonSettings);
+            return Utf8NoBom.GetBytes(instring);
+        }
+
+    }
+}
